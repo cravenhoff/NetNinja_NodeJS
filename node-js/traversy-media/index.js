4ff76e598a62e7837f3b995ec14f43ba5b461570
cravenhoff <crystalkewe@gmail.com>
2020-10-18 20:44:32 +1000

    NodeJS Crash Course - Traversy Media: Commit Remaining Files

---
+++ node-js/traversy-media/index.js
@@ -0,0 +1,105 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+// Create new server object
+const server = http.createServer((req, res) => {
+    /*  
+        --- Inefficient, but bare Node.JS Code / Logic: ---
+
+        // If UR "/", retrieves and displays the index.html file
+        if(req.url === "/") {
+            fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+                if(err) {
+                    throw err;
+                }
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(content);
+            });
+        }
+
+        // If UR "/about", retrieves and displays the about.html file
+        if(req.url === "/about") {
+            fs.readFile(path.join(__dirname, "public", "about.html"), (err, content) => {
+                if(err) {
+                    throw err;
+                }
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(content);
+            });
+        }
+
+        // Outputs JSON Content
+        if(req.url === "/api/users") {
+            const users = [
+                { name: "Mike Fanuka", age: 40 },
+                { name: "John Doe", age: 30 }
+            ];
+
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(users));
+        }
+
+    */
+
+    // Making the file path dynamic
+    // Build file path
+    let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
+   
+    // Extension file
+    let extName = path.extname(filePath);
+
+    // Initial content type
+    let contentType = "text/html";
+
+    // Check extension and set content type
+    switch(extName) {
+        case ".js" :
+            contentType = "text/javascript";
+            break;
+
+        case ".css" :
+            contentType = "text/css";
+            break;
+
+        case ".json" :
+            contentType = "application/json";
+            break;
+
+        case ".png" :
+            contentType = "image/png";
+            break;
+
+        case ".jpg" :
+            contentType = "image/jpg";
+            break;
+                
+    }
+
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if(err.code === "ENOENT") {
+                // Page not found
+                fs.readFile(path.join(__dirname, "public", "404.html"), (err, content) => {
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    res.end(content, "utf8");
+                })
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, {"Content-Type": contentType});
+            res.end(content, "utf8");
+        }
+    })
+
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
