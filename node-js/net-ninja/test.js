6b656a10cc8db1b529beaf11875c285c8025207e
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-10-18 21:17:02 +1000

    Update test.js

--- node-js/net-ninja/test.js
+++ node-js/net-ninja/test.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-=======
+
 // Introduction to Node.JS Lesson
 
 // Create a sample variable and output it to the console/terminal
 const name = "Sitoshi";
 console.log(name);
->>>>>>> 6216bd657cd78aeb83496c3e9be46390d1bb7d93
