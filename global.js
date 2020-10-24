716ac636b781ca07cf712703ff22cf94d8279a03
cravenhoff <crystalkewe@gmail.com>
2020-10-25 01:54:27 +1000

    Intro to the Node Global Object.

---
+++ global.js
@@ -0,0 +1,13 @@
+// Global object
+// console.log(global);
+
+// Runs once after the specified time
+global.setTimeout(() => {
+    console.log("in the timeout");
+    clearInterval(int);
+}, 3000);
+
+// Runs after a certain interval continuously
+const int = setInterval(() => {
+    console.log("in the interval");
+}, 1000);
