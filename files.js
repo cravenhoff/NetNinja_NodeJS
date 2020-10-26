f023b022c99746fc45aec4f3bc5829d163d2ece1
cravenhoff <crystalkewe@gmail.com>
2020-10-26 16:32:32 +1000

    Reading a file using the FS Module.

--- files.js
+++ files.js
@@ -1,7 +1,17 @@
 const fs = require("fs");
 
 // Reading files
+fs.readFile("./docs/blog1.txt", (err, data) => {
+    if(err) {
+        console.log(err);
+    }
 
+    // sends out a buffer instead of the actual contents of the file it's reading
+    console.log(data.toString()); // toString() is used to convert the buffer into a string
+});
+
+// Showing how the fs methods are "asynchronous"
+console.log("last line...");
 
 // Writing files
 
